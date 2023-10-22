fa3a9a53f5023bad8ab0410e15dee16e218865b1
siirene <eus2un@virginia.edu>
2023-10-21 20:07:00 -0400

    played around with sprite movement/collisions...(with prof graham lol)

---
+++ Girl Game/Assets/Scenes/ChairInteraction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChairInteraction : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.LogError("Clicked.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Girl Game/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        if (moveDirection != Vector3.zero)
+        {
+            // Move the player
+            transform.Translate(moveDirection * speed * Time.deltaTime);
+        }
+    }
+
+    // OnMouseDown is called when the user has pressed the mouse button while over the GUIElement or Collider
+    private void OnMouseDown()
+    {
+        Debug.LogError("Clicked.");
+    }
+
+    // OnCollisionEnter2D is called when this collider2D/rigidbody2D has begun touching another rigidbody2D/collider2D (2D physics only)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
+
