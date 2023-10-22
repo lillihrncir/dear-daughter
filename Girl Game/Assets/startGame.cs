977bc3efb4fa84b6a0a7bc4be597d3373db28efb
siirene <eus2un@virginia.edu>
2023-10-22 12:25:31 -0400

    blink start

--- Girl Game/Assets/startGame.cs
+++ Girl Game/Assets/startGame.cs
@@ -1,31 +1,97 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class startGame : MonoBehaviour
+//{
+//    public float blinkInterval = 0.5f; // Time interval for blinking in seconds
+//    private Text textComponent;
+
+//    private void Start()
+//    {
+//        textComponent = GetComponent<Text>();
+//        StartCoroutine(Blink());
+//    }
+
+//    private IEnumerator Blink()
+//    {
+//        while (true) // Infinite loop for continuous blinking
+//        {
+//            textComponent.enabled = !textComponent.enabled; // Toggle visibility
+
+//            yield return new WaitForSeconds(blinkInterval);
+//        }
+//    }
+//}
+
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Text;
+using System;
 
-public class BlinkTextMesh : MonoBehaviour
+public class UITextTypeWriter : MonoBehaviour
 {
-    public TextMesh textMesh;
-    public float blinkInterval = 0.5f; // Time interval for blinking in seconds
+    public Text text;
+    public bool playOnAwake = true;
+    public float delayToStart;
+    public float delayBetweenChars = 0.125f;
+    public float delayAfterPunctuation = 0.5f;
 
-    private void Start()
+    private string story;
+    private float originDelayBetweenChars;
+    private bool lastCharPunctuation = false;
+    private char charComma;
+    private char charPeriod;
+
+    void Awake()
     {
-        StartCoroutine(Blink());
+        text = GetComponent<Text>();
+        originDelayBetweenChars = delayBetweenChars;
+
+        charComma = Convert.ToChar(44);
+        charPeriod = Convert.ToChar(46);
+
+        if (playOnAwake)
+        {
+            ChangeText(text.text, delayToStart);
+        }
     }
 
-    private IEnumerator Blink()
+    //Update text and start typewriter effect
+    public void ChangeText(string textContent, float delayBetweenChars = 0f)
     {
-        Material textMaterial = textMesh.GetComponent<Renderer>().material;
+        StopCoroutine(PlayText()); //stop Coroutime if exist
+        story = textContent;
+        text.text = ""; //clean text
+        Invoke("Start_PlayText", delayBetweenChars); //Invoke effect
+    }
 
-        while (true) // Infinite loop for continuous blinking
+    void Start_PlayText()
+    {
+        StartCoroutine(PlayText());
+    }
+
+    IEnumerator PlayText()
+    {
+
+        foreach (char c in story)
         {
-            Color textColor = textMaterial.color;
+            delayBetweenChars = originDelayBetweenChars;
 
-            // Toggle the alpha channel to control visibility
-            textColor.a = Mathf.Approximately(textColor.a, 0f) ? 1f : 0f;
-            textMaterial.color = textColor;
+            if (lastCharPunctuation)  //If previous character was a comma/period, pause typing
+            {
+                yield return new WaitForSeconds(delayBetweenChars = delayAfterPunctuation);
+                lastCharPunctuation = false;
+            }
 
-            yield return new WaitForSeconds(blinkInterval);
+            if (c == charComma || c == charPeriod)
+            {
+                lastCharPunctuation = true;
+            }
+
+            text.text += c;
+            yield return new WaitForSeconds(delayBetweenChars);
         }
     }
 }
-
